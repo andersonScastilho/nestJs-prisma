a1df7fba5bee6e4b75d53b13a74000943397753a
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-11-16 13:23:48 -0300

    Adicionando upload de multiplos arquivos

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,6 +6,7 @@ import {
   UseInterceptors,
   UploadedFile,
   BadRequestException,
+  UploadedFiles,
 } from '@nestjs/common';
 import { AuthLoginDto } from './dto/auth-login.dto';
 import { AuthRegisterDto } from './dto/auth-register.dto';
@@ -14,7 +15,11 @@ import { AuthResetDto } from './dto/auth-reset.dto';
 import { AuthService } from './auth.service';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { User } from 'src/decorators/user.decorator';
-import { FileInterceptor } from '@nestjs/platform-express';
+import {
+  FileInterceptor,
+  FilesInterceptor,
+  FileFieldsInterceptor,
+} from '@nestjs/platform-express';
 import { join } from 'path';
 import { FileService } from 'src/file/file.service';
 @Controller('auth')
@@ -63,5 +68,80 @@ export class AuthController {
     }
 
     return { success: true };
+  }
+
+  @UseGuards(AuthGuard)
+  @UseInterceptors(FilesInterceptor('files'))
+  @Post('files')
+  async uploadFiles(
+    @User() user,
+    @UploadedFiles() files: Express.Multer.File[],
+  ) {
+    try {
+      files.forEach(async (file, index) => {
+        const personPath = join(
+          __dirname,
+          '..',
+          '..',
+          'storage',
+          'photos',
+          `photo-${user.id}-${index}.png`,
+        );
+
+        await this.fileService.upload(file, personPath);
+      });
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+
+    return files;
+  }
+
+  @UseGuards(AuthGuard)
+  @UseInterceptors(
+    FileFieldsInterceptor([
+      { name: 'photo', maxCount: 1 },
+      { name: 'documents', maxCount: 10 },
+    ]),
+  )
+  @Post('files-fields')
+  async uploadFilesFields(
+    @User() user,
+    @UploadedFiles()
+    files: { photo: Express.Multer.File; documents: Express.Multer.File[] },
+  ) {
+    const path = join(
+      __dirname,
+      '..',
+      '..',
+      'storage',
+      'photos',
+      `photo-${user.id}.png`,
+    );
+
+    try {
+      await this.fileService.upload(files.photo, path);
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+
+    try {
+      files.documents.forEach(async (document, index) => {
+        const personPath = join(
+          __dirname,
+          '..',
+          '..',
+          'storage',
+          'photos',
+          `photo-${user.id}-${index}.png`,
+        );
+
+        await this.fileService.upload(document, personPath);
+      });
+    } catch (e) {
+      throw new BadRequestException(e);
+    }
+
+    return files;
   }
 }
