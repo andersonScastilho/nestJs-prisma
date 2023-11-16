06c2c463bd6d6785f9b42ead0826c188fe99c5b0
Anderson Leonardo <anderson.scastilho@hotmail.com>
2023-11-16 10:34:26 -0300

    Adicionando limit de conexão, por ip

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,27 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { AuthModule } from './auth/auth.module';
-
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 @Module({
-  imports: [forwardRef(() => UserModule), forwardRef(() => AuthModule)],
+  imports: [
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 10,
+      },
+    ]),
+    forwardRef(() => UserModule),
+    forwardRef(() => AuthModule),
+  ],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  exports: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,9 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.enableCors();
 
+  app.useGlobalPipes(new ValidationPipe());
   app.enableShutdownHooks();
 
   await app.listen(3000);
